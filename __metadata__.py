691362ce311988413e180894375343fb46cab798
Trevor Bača <trevorbaca@gmail.com>
2014-07-24 11:00:38 -0400

    Added draft of front cover.

--- __metadata__.py
+++ __metadata__.py
@@ -4,8 +4,8 @@ import collections
 
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-012'),
-    ('forces_tagline', 'for eight players'),
-    ('paper_dimensions', '17 x 11'),
+    ('forces_tagline', 'for seven players'),
+    ('paper_dimensions', '17 x 11 in'),
     ('title', 'Krummzeit'),
     ('year', 2014),
     ])

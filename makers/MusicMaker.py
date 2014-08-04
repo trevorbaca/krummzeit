769bc8ffde508e46970a6355c509def08e689a3c
Trevor Bača <trevorbaca@gmail.com>
2014-08-04 15:57:24 -0400

    Added music-maker.

---
+++ makers/MusicMaker.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+class MusicMaker(abctools.AbjadObject):
+   r'''Krummzeit music-maker.
+   '''
+
+    ### INITIALIZER ###    
+
+    def __init__(self):
+        pass
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self):
+        r'''Calls Krummzeit music-maker.
+
+        Returns music. Probably as a selection.
+        '''
+        pass

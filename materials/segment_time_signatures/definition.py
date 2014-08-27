f8e58a9b9c7bd3bc0f8b74abc78894391eb1e49a
Trevor Bača <trevorbaca@gmail.com>
2014-08-27 11:14:42 -0400

    Built tempi & time signatures [E]. Wrote [E] (v1).

--- materials/segment_time_signatures/definition.py
+++ materials/segment_time_signatures/definition.py
@@ -16,3 +16,10 @@ assert len(time_signatures) == 64
 time_signatures[0:0] = time_signatures[:11]
 assert len(time_signatures) == 75
 segment_time_signatures['B'] = time_signatures
+
+
+### E ###
+time_signatures = time_signature_inventory[3:5]
+time_signatures = sequencetools.flatten_sequence(time_signatures)
+assert len(time_signatures) == 48
+segment_time_signatures['E'] = time_signatures

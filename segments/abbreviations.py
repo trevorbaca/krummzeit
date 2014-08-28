21493540562998f05861d1c10785fb548d73bf31
Trevor Bača <trevorbaca@gmail.com>
2014-08-28 19:29:11 -0400

    Built cl, va, vc timepoints [F] (v4).

--- segments/abbreviations.py
+++ segments/abbreviations.py
@@ -5,6 +5,7 @@ from krummzeit import materials
 ### INSTRUMENTS ###
 
 e_flat_clarinet = materials.performer_inventory.get_instrument('cl. e-flat')
+clarinet = materials.performer_inventory.get_instrument('cl. in B-flat')
 bass_clarinet = materials.performer_inventory.get_instrument('bass cl.')
 piano = materials.performer_inventory.get_instrument('piano')
 piano._default_scope = 'PianoMusicStaff'

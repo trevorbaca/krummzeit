e877d72f358cd84f81e1b74292a02ad846b13186
Trevor Bača <trevorbaca@gmail.com>
2014-08-27 21:39:30 -0400

    Extended tempo inventory.
    Added tempo 108 subito 72.

--- materials/named_tempo_inventory/output.py
+++ materials/named_tempo_inventory/output.py
@@ -5,31 +5,10 @@ from abjad import *
 named_tempo_inventory = datastructuretools.TypedOrderedDict(
     [
         (
-            '72',
+            '36',
             indicatortools.Tempo(
                 duration=durationtools.Duration(1, 4),
-                units_per_minute=72,
-                ),
-            ),
-        (
-            '108',
-            indicatortools.Tempo(
-                duration=durationtools.Duration(1, 4),
-                units_per_minute=108,
-                ),
-            ),
-        (
-            '90',
-            indicatortools.Tempo(
-                duration=durationtools.Duration(1, 4),
-                units_per_minute=90,
-                ),
-            ),
-        (
-            '135',
-            indicatortools.Tempo(
-                duration=durationtools.Duration(1, 4),
-                units_per_minute=135,
+                units_per_minute=36,
                 ),
             ),
         (
@@ -66,10 +45,102 @@ named_tempo_inventory = datastructuretools.TypedOrderedDict(
                 ),
             ),
         (
-            '36',
+            '72',
             indicatortools.Tempo(
                 duration=durationtools.Duration(1, 4),
-                units_per_minute=36,
+                units_per_minute=72,
+                ),
+            ),
+        (
+            '72/108',
+            indicatortools.Tempo(
+                duration=durationtools.Duration(1, 4),
+                units_per_minute=108,
+                markup=markuptools.Markup(
+                    contents=(
+                        markuptools.MarkupCommand(
+                            'override',
+                            schemetools.SchemePair('padding', 0.45),
+                            markuptools.MarkupCommand(
+                                'parenthesize',
+                                markuptools.MarkupCommand(
+                                    'line',
+                                    [
+                                        markuptools.MarkupCommand(
+                                            'smaller',
+                                            markuptools.MarkupCommand(
+                                                'general-align',
+                                                schemetools.Scheme(
+                                                    'Y'
+                                                    ),
+                                                schemetools.Scheme(
+                                                    'DOWN'
+                                                    ),
+                                                markuptools.MarkupCommand(
+                                                    'note-by-number',
+                                                    2,
+                                                    0,
+                                                    1
+                                                    )
+                                                )
+                                            ),
+                                        markuptools.MarkupCommand(
+                                            'upright',
+                                            ' = 72'
+                                            ),
+                                        ]
+                                    )
+                                )
+                            ),
+                        markuptools.MarkupCommand(
+                            'italic',
+                            'subito'
+                            ),
+                        markuptools.MarkupCommand(
+                            'smaller',
+                            markuptools.MarkupCommand(
+                                'general-align',
+                                schemetools.Scheme(
+                                    'Y'
+                                    ),
+                                schemetools.Scheme(
+                                    'DOWN'
+                                    ),
+                                markuptools.MarkupCommand(
+                                    'note-by-number',
+                                    2,
+                                    0,
+                                    1
+                                    )
+                                )
+                            ),
+                        markuptools.MarkupCommand(
+                            'upright',
+                            ' = 108'
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        (
+            '90',
+            indicatortools.Tempo(
+                duration=durationtools.Duration(1, 4),
+                units_per_minute=90,
+                ),
+            ),
+        (
+            '108',
+            indicatortools.Tempo(
+                duration=durationtools.Duration(1, 4),
+                units_per_minute=108,
+                ),
+            ),
+        (
+            '135',
+            indicatortools.Tempo(
+                duration=durationtools.Duration(1, 4),
+                units_per_minute=135,
                 ),
             ),
         ]

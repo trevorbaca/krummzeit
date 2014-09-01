8124f57b0780b502415b674bcb44f591f47938b2
Trevor Bača <trevorbaca@gmail.com>
2014-09-01 19:39:58 -0400

    Reworked pitch specifier.

--- makers/PitchSpecifier.py
+++ makers/PitchSpecifier.py
@@ -11,14 +11,14 @@ class PitchSpecifier(abctools.AbjadObject):
 
             >>> import krummzeit
             >>> handler = krummzeit.makers.PitchSpecifier(
-            ...     pitches=[7, 1, 3, 4, 5, 11],
+            ...     source=[7, 1, 3, 4, 5, 11],
             ...     )
 
         ::
 
             >>> print(format(handler))
             krummzeit.makers.PitchSpecifier(
-                pitches=[
+                source=[
                     pitchtools.NamedPitch("g'"),
                     pitchtools.NamedPitch("cs'"),
                     pitchtools.NamedPitch("ef'"),
@@ -33,26 +33,57 @@ class PitchSpecifier(abctools.AbjadObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
-        '_pitches',
+        '_operators_',
+        '_reverse',
+        '_source',
+        '_start_index',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
-        pitches=None,
+        operators_=None,
+        reverse=False,
+        source=None,
+        start_index=0,
         ):
         from abjad.tools import pitchtools
-        if pitches is not None:
-            pitches = tuple(pitches)
-            pitches = [pitchtools.NamedPitch(_) for _ in pitches]
-        self._pitches = pitches
+        if operators_ is not None:
+            operators_ = tuple(operators_)
+        self._operators_ = operators_
+        assert isinstance(reverse, bool), repr(reverse)
+        self._reverse = reverse
+        if source is not None:
+            source = tuple(source)
+            source = [pitchtools.NamedPitch(_) for _ in source]
+        self._source = source
+        assert isinstance(start_index, int), repr(start_index)
+        self._start_index = start_index
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _storage_format_specification(self):
+        from abjad.tools import systemtools
+        manager = systemtools.StorageFormatManager
+        keyword_argument_names = \
+            manager.get_signature_keyword_argument_names(self)
+        keyword_argument_names = list(keyword_argument_names)
+        if self.reverse == False:
+            keyword_argument_names.remove('reverse')
+        if self.start_index == 0:
+            keyword_argument_names.remove('start_index')
+        return systemtools.StorageFormatSpecification(
+            self,
+            keyword_argument_names=keyword_argument_names,
+            )
 
     ### PUBLIC PROPERTIES ###
 
     @property
-    def pitches(self):
-        r'''Gets pitches of pitch-handler.
+    def operators_(self):
+        r'''Gets operators of pitch-handler.
 
         ..  container:: example
 
@@ -60,12 +91,60 @@ class PitchSpecifier(abctools.AbjadObject):
 
                 >>> import krummzeit
                 >>> handler = krummzeit.makers.PitchSpecifier(
-                ...     pitches=[7, 1, 3, 4, 5, 11],
+                ...     operators_=[
+                ...         ],
+                ...     source=[7, 1, 3, 4, 5, 11],
                 ...     )
 
             ::
 
-                >>> for pitch in handler.pitches:
+                >>> handler.operators_
+                ()
+
+        Set to operators or none.
+        '''
+        return self._operators_
+
+    @property
+    def reverse(self):
+        r'''Is true when pitch specifier should read pitches in reverse.
+        Otherwise false.
+
+        ..  container:: example
+
+            ::
+
+                >>> import krummzeit
+                >>> handler = krummzeit.makers.PitchSpecifier(
+                ...     reverse=True,
+                ...     source=[7, 1, 3, 4, 5, 11],
+                ...     start_index=-1,
+                ...     )
+
+            ::
+
+                >>> handler.reverse
+                True
+
+        Set to true or false.
+        '''
+        return self._reverse
+
+    @property
+    def source(self):
+        r'''Gets source of pitch specifier.
+
+        ..  container:: example
+
+            ::
+
+                >>> handler = krummzeit.makers.PitchSpecifier(
+                ...     source=[7, 1, 3, 4, 5, 11],
+                ...     )
+
+            ::
+
+                >>> for pitch in handler.source:
                 ...     pitch
                 NamedPitch("g'")
                 NamedPitch("cs'")
@@ -74,6 +153,31 @@ class PitchSpecifier(abctools.AbjadObject):
                 NamedPitch("f'")
                 NamedPitch("b'")
 
-        Set to pitches or none.
+        Set to pitch source or none.
         '''
-        return self._pitches
+        return self._source
+
+
+    @property
+    def start_index(self):
+        r'''Gets start index of pitch-handler.
+
+        ..  container:: example
+
+            ::
+
+                >>> import krummzeit
+                >>> handler = krummzeit.makers.PitchSpecifier(
+                ...     reverse=True,
+                ...     source=[7, 1, 3, 4, 5, 11],
+                ...     start_index=-1,
+                ...     )
+
+            ::
+
+                >>> handler.start_index
+                -1
+
+        Set to integer or none.
+        '''
+        return self._start_index

2c92a09b9c5741f42039822527fb5ee8e4c66943
Trevor Bača <trevorbaca@gmail.com>
2014-09-05 11:14:03 -0400

    Articulated [A] (v8).

--- makers/SegmentMaker.py
+++ makers/SegmentMaker.py
@@ -58,7 +58,7 @@ class SegmentMaker(makertools.SegmentMaker):
         self._annotate_stages()
         self._interpret_music_makers()
         self._interpret_music_handlers()
-        self._move_clefs_from_notes_back_to_rests()
+        #self._move_clefs_from_notes_back_to_rests()
         self._move_instruments_from_notes_back_to_rests()
         self._attach_instrument_to_first_leaf()
         self._move_untuned_percussion_markup_to_first_note()

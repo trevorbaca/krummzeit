6dc494fd60182a671094f19068bc642190e97b2f
Trevor Bača <trevorbaca@gmail.com>
2014-09-28 21:37:22 -0400

    Upated pitch-class definition files.

--- materials/indigo_pitch_classes/definition.py
+++ materials/indigo_pitch_classes/definition.py
@@ -1,10 +1,9 @@
 # -*- encoding: utf-8 -*-
 from abjad import *
 import baca
-import score_manager_library
 
 
-indigo_pitch_classes = baca.score_manager_library.makers.ZaggedPitchClassMaker(
+indigo_pitch_classes = baca.library.makers.ZaggedPitchClassMaker(
     pc_cells=[
         [7, 1, 3, 4, 5, 11],
         [3, 5, 6, 7],

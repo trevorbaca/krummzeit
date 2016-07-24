3716d99407f2d459934a690cc8ecc21f4cbe653c
Trevor Bača <trevor.baca@gmail.com>
2016-07-24 17:09:35 -0500

    Updated metadata.

--- krummzeit/__metadata__.py
+++ krummzeit/__metadata__.py
@@ -5,7 +5,7 @@ import collections
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-012'),
     ('forces_tagline', 'for seven players'),
-    ('paper_dimensions', '17 x 11 in'),
+    ('paper_size', '17 x 11 in'),
     ('price', '\\euro 66 / \\$84'),
     ('title', 'Krummzeit'),
     ('year', 2014),
--- krummzeit/segments/segment_j/__metadata__.py
+++ krummzeit/segments/segment_j/__metadata__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from abjad import *
+import abjad
 
 
-metadata = datastructuretools.TypedOrderedDict(
+metadata = abjad.datastructuretools.TypedOrderedDict(
     [
         ('name', 'J'),
         ('segment_count', 11),

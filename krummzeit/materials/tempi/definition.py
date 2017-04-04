1b7b8a90b178522309735704be9fc71801748518
Trevor Bača <trevor.baca@gmail.com>
2017-04-04 11:38:04 -0500

    Updated indicators.

--- krummzeit/materials/tempi/definition.py
+++ krummzeit/materials/tempi/definition.py
@@ -2,7 +2,7 @@
 import abjad
 
 
-tempi = abjad.TempoInventory(
+tempi = abjad.indicatortools.TempoInventory(
     [
         abjad.Tempo(
             reference_duration=abjad.durationtools.Duration(1, 4),

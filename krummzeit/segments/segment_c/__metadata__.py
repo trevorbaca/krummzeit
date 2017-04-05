6637a3e8f714792d5b6f44fda14799dcca775f73
Trevor Bača <trevor.baca@gmail.com>
2017-04-05 13:18:14 -0500

    Refactored score template.

--- krummzeit/segments/segment_c/__metadata__.py
+++ krummzeit/segments/segment_c/__metadata__.py
@@ -4,6 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 89),
         ('name', 'C'),
         ('segment_count', 11),
         ('segment_number', 3),

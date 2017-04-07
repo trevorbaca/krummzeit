57453983cae99d850755865290da567a021be06c
Trevor Bača <trevor.baca@gmail.com>
2017-04-07 10:39:01 -0700

    Rebuilding [F].

--- krummzeit/segments/segment_f/__metadata__.py
+++ krummzeit/segments/segment_f/__metadata__.py
@@ -4,6 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 200),
         ('name', 'F'),
         ('segment_count', 11),
         ('segment_number', 6),

f4c70a668e1e249dfd0aff4af6cc6bc686fa6c51
Trevor Bača <trevor.baca@gmail.com>
2017-04-08 12:06:32 -0700

    Rebuilt [I].

--- krummzeit/segments/segment_i/__metadata__.py
+++ krummzeit/segments/segment_i/__metadata__.py
@@ -4,7 +4,30 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        (
+            'end_clefs_by_staff',
+            abjad.datastructuretools.TypedOrderedDict(
+                [
+                    ('Cello Music Staff', None),
+                    ('Clarinet Music Staff', None),
+                    ('Oboe Music Staff', None),
+                    ('Percussion Staff', 'treble'),
+                    ('Piano Music Staff', 'treble'),
+                    ('Viola Music Staff', None),
+                    ('Violin Music Staff', None),
+                    ]
+                ),
+            ),
+        (
+            'end_instruments_by_context',
+            abjad.datastructuretools.TypedOrderedDict(
+                []
+                ),
+            ),
+        ('end_tempo', None),
+        ('end_time_signature', '7/4'),
         ('first_bar_number', 281),
+        ('measure_count', 10),
         ('name', 'I'),
         ('segment_count', 11),
         ('segment_number', 9),

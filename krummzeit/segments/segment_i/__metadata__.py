ef077dd96b2eac8c70354c7cc140aeea86707d47
Trevor Bača <trevor.baca@gmail.com>
2017-04-07 19:01:12 -0700

    Rerendered segments.

--- krummzeit/segments/segment_i/__metadata__.py
+++ krummzeit/segments/segment_i/__metadata__.py
@@ -4,6 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 281),
         ('name', 'I'),
         ('segment_count', 11),
         ('segment_number', 9),

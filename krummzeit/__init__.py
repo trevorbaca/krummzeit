f1ce64a7931021490d5ab84734cd3fc5348503df
Trevor Bača <trevor.baca@gmail.com>
2017-04-29 19:51:32 -0500

    Cleaned up package initializer.

--- krummzeit/__init__.py
+++ krummzeit/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.5.')
+del distutils
+del platform
 from krummzeit import tools
 from krummzeit import materials
 from krummzeit import segments

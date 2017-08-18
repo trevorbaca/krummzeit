be04f466665c22a164632ece058058be39dde318
Trevor Bača <trevor.baca@gmail.com>
2017-08-18 15:45:47 -0500

    Cleaned up final markup.

--- krummzeit/tools/final_markup.py
+++ krummzeit/tools/final_markup.py
@@ -3,10 +3,53 @@ import abjad
 
 def final_markup():
     r'''Makes final markup.
+
+    ::
+
+        >>> import krummzeit
+
+    ..  container:: example
+
+        ::
+
+            >>> markup = krummzeit.final_markup()
+            >>> show(markup) # doctest: +SKIP
+
+        ..  docs::
+
+            >>> f(markup)
+            _ \markup {
+                \italic
+                    \right-column
+                        {
+                            \line
+                                {
+                                    "Cambridge, MA."
+                                }
+                            \line
+                                {
+                                    May
+                                    \hspace
+                                        #0.75
+                                    –
+                                    \hspace
+                                        #0.75
+                                    August
+                                    2014.
+                                }
+                        }
+                }
+
     '''
-    place = abjad.Markup('Cambridge, MA.')
-    date = abjad.Markup('May - August 2014.')
-    final_markup = abjad.Markup.right_column([place, date])
-    final_markup = final_markup.italic()
-    final_markup = abjad.new(final_markup, direction=Down)
-    return final_markup
+    place = ['Cambridge, MA.']
+    place = r' \hspace #0.75 – \hspace #0.75 '.join(place)
+    place = abjad.Markup(place)
+    place = abjad.Markup.line([place])
+    date = ['May', 'August 2014.']
+    date = r' \hspace #0.75 – \hspace #0.75 '.join(date)
+    date = abjad.Markup(date)
+    date = abjad.Markup.line([date])
+    markup = abjad.Markup.right_column([place, date])
+    markup = markup.italic()
+    markup = abjad.new(markup, direction=abjad.Down)
+    return markup

92829df95b41d4808a4130fb9e894054b307a59e
Trevor Bača <trevor.baca@gmail.com>
2017-09-28 18:36:31 -0500

    Integrated instrument dictionary.

--- krummzeit/materials/named_tempi/definition.py
+++ krummzeit/materials/named_tempi/definition.py
@@ -2,14 +2,16 @@ import abjad
 from krummzeit.materials.tempi.definition import tempi
 
 
-named_tempi = abjad.TypedOrderedDict([
-    ('36', tempi[5]),
-    ('45', tempi[8]),
-    ('67.5', tempi[4]),
-    ('72', tempi[0]),
-    ('72/108', tempi[6]),
-    ('90', tempi[2]),
-    ('108', tempi[1]),
-    ('135', tempi[3]),
-    ('144', tempi[7]),
-    ])
+#named_tempi = abjad.TypedOrderedDict([
+#    ('36', tempi[5]),
+#    ('45', tempi[8]),
+#    ('67.5', tempi[4]),
+#    ('72', tempi[0]),
+#    ('72/108', tempi[6]),
+#    ('90', tempi[2]),
+#    ('108', tempi[1]),
+#    ('135', tempi[3]),
+#    ('144', tempi[7]),
+#    ])
+
+named_tempi = tempi
--- krummzeit/materials/tempi/definition.py
+++ krummzeit/materials/tempi/definition.py
@@ -1,124 +1,160 @@
 import abjad
 
 
-tempi = abjad.MetronomeMarkList(
+tempi = abjad.MetronomeMarkDictionary(
     [
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=72,
-            ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=108,
-            ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=90,
-            ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=135,
-            ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=67.5,
-            custom_markup=abjad.Markup(
-                contents=(
-                    abjad.MarkupCommand(
-                        'smaller',
-                        abjad.MarkupCommand(
-                            'general-align',
-                            abjad.Scheme(
-                                'Y'
-                                ),
-                            abjad.Scheme(
-                                'DOWN'
-                                ),
-                            abjad.MarkupCommand(
-                                'note-by-number',
-                                2,
-                                0,
-                                1
-                                )
-                            )
-                        ),
-                    abjad.MarkupCommand('upright', ' = 67.5'),
-                    ),
+        (
+            '36',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=36,
                 ),
             ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=36,
+        (
+            '45',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=45,
+                ),
             ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=73,
-            custom_markup=abjad.Markup(
-                contents=(
-                    abjad.MarkupCommand(
-                        'override',
-                        abjad.SchemePair(('padding', 0.45)),
+        (
+            '67.5',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=67.5,
+                custom_markup=abjad.Markup(
+                    contents=[
                         abjad.MarkupCommand(
-                            'parenthesize',
+                            'smaller',
                             abjad.MarkupCommand(
-                                'line',
-                                [
-                                    abjad.MarkupCommand(
-                                        'smaller',
-                                        abjad.MarkupCommand(
-                                            'general-align',
-                                            abjad.Scheme(
-                                                'Y'
-                                                ),
-                                            abjad.Scheme(
-                                                'DOWN'
-                                                ),
-                                            abjad.MarkupCommand(
-                                                'note-by-number',
-                                                2,
-                                                0,
-                                                1
-                                                )
-                                            )
-                                        ),
-                                    abjad.MarkupCommand('upright', ' = 72'),
-                                    ],
-                                ),
+                                'general-align',
+                                abjad.Scheme(
+                                    'Y'
+                                    ),
+                                abjad.Scheme(
+                                    'DOWN'
+                                    ),
+                                abjad.MarkupCommand(
+                                    'note-by-number',
+                                    2,
+                                    0,
+                                    1
+                                    )
+                                )
                             ),
-                        ),
-                    abjad.MarkupCommand(
-                        'italic',
-                        'subito',
-                        ),
-                    abjad.MarkupCommand(
-                        'smaller',
                         abjad.MarkupCommand(
-                            'general-align',
-                            abjad.Scheme(
-                                'Y'
-                                ),
-                            abjad.Scheme(
-                                'DOWN'
-                                ),
-                            abjad.MarkupCommand(
-                                'note-by-number',
-                                2,
-                                0,
-                                1
-                                )
-                            )
-                        ),
-                    abjad.MarkupCommand('upright', ' = 108'),
+                            'upright',
+                            ' = 67.5'
+                            ),
+                        ],
                     ),
                 ),
             ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=144,
+        (
+            '72',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=72,
+                ),
             ),
-        abjad.MetronomeMark(
-            reference_duration=abjad.Duration(1, 4),
-            units_per_minute=45,
+        (
+            '72/108',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=73,
+                custom_markup=abjad.Markup(
+                    contents=[
+                        abjad.MarkupCommand(
+                            'override',
+                            abjad.SchemePair(('padding', 0.45)),
+                            abjad.MarkupCommand(
+                                'parenthesize',
+                                abjad.MarkupCommand(
+                                    'line',
+                                    [
+                                        abjad.MarkupCommand(
+                                            'smaller',
+                                            abjad.MarkupCommand(
+                                                'general-align',
+                                                abjad.Scheme(
+                                                    'Y'
+                                                    ),
+                                                abjad.Scheme(
+                                                    'DOWN'
+                                                    ),
+                                                abjad.MarkupCommand(
+                                                    'note-by-number',
+                                                    2,
+                                                    0,
+                                                    1
+                                                    )
+                                                )
+                                            ),
+                                        abjad.MarkupCommand(
+                                            'upright',
+                                            ' = 72'
+                                            ),
+                                        ]
+                                    )
+                                )
+                            ),
+                        abjad.MarkupCommand(
+                            'italic',
+                            'subito'
+                            ),
+                        abjad.MarkupCommand(
+                            'smaller',
+                            abjad.MarkupCommand(
+                                'general-align',
+                                abjad.Scheme(
+                                    'Y'
+                                    ),
+                                abjad.Scheme(
+                                    'DOWN'
+                                    ),
+                                abjad.MarkupCommand(
+                                    'note-by-number',
+                                    2,
+                                    0,
+                                    1
+                                    )
+                                )
+                            ),
+                        abjad.MarkupCommand(
+                            'upright',
+                            ' = 108'
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        (
+            '90',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=90,
+                ),
+            ),
+        (
+            '108',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=108,
+                ),
+            ),
+        (
+            '135',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=135,
+                ),
+            ),
+        (
+            '144',
+            abjad.MetronomeMark(
+                reference_duration=abjad.Duration(1, 4),
+                units_per_minute=144,
+                ),
             ),
         ]
     )

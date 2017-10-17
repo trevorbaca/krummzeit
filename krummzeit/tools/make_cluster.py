5890718cf57db3a9089975f4873eba05b3090f52
Trevor Bača <trevor.baca@gmail.com>
2017-10-17 11:14:37 -0500

    Integrated cluster changes.

--- krummzeit/tools/make_cluster.py
+++ krummzeit/tools/make_cluster.py
@@ -7,20 +7,9 @@ def make_cluster(flavor):
 
     Set `flavor` to 'tenor', 'low' or 'harpsichord'.
     '''
-    if flavor == 'harpsichord':
-        return baca.clusters(
-            start_pitch='D4',
-            widths=[4],
-            )
-    elif flavor == 'low':
-        return baca.clusters(
-            start_pitch='C1',
-            widths=[7],
-            )
-    elif flavor == 'tenor':
-        return baca.clusters(
-            start_pitch='A2',
-            widths=[4],
-            )
-    else:
-        raise ValueError(flavor)
+    clusters = {
+        'harpsichord': baca.clusters([4], start_pitch='D4'),
+        'low': baca.clusters([7], start_pitch='C1'),
+        'tenor': baca.clusters([4], start_pitch='A2'),
+        }
+    return clusters[flavor]

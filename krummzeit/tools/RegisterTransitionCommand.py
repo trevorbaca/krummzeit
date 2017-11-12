137a0096d3930ca23928a14927456c22b3415a46
Trevor Bača <trevor.baca@gmail.com>
2017-11-12 13:21:31 -0600

    Cleaning up docs.

--- krummzeit/tools/RegisterTransitionCommand.py
+++ krummzeit/tools/RegisterTransitionCommand.py
@@ -30,8 +30,7 @@ class RegisterTransitionCommand(baca.Command):
         ...         ),
         ...     )
 
-        >>> result = segment_maker.run(environment='docs')
-        >>> lilypond_file, metadata = result
+        >>> lilypond_file = segment_maker.run(environment='docs')
         >>> abjad.show(lilypond_file) # doctest: +SKIP
 
         ..  docs::

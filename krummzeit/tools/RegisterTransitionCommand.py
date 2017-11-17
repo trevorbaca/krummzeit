ba91133f7992e39b353a531d623ac8964a4b8fdb
Trevor Bača <trevor.baca@gmail.com>
2017-11-17 10:50:19 -0600

    Integrating format changes.

--- krummzeit/tools/RegisterTransitionCommand.py
+++ krummzeit/tools/RegisterTransitionCommand.py
@@ -39,50 +39,70 @@ class RegisterTransitionCommand(baca.Command):
             \context Score = "Score" <<
                 \context GlobalContext = "Global Context" <<
                     \context GlobalSkips = "Global Skips" {
-                        % measure 1
+            <BLANKLINE>
+                        %%% Global Skips [measure 1] %%%
                         \time 4/8
                         s1 * 1/2
-                        % measure 2
+            <BLANKLINE>
+                        %%% Global Skips [measure 2] %%%
                         \time 3/8
                         s1 * 3/8
-                        % measure 3
+            <BLANKLINE>
+                        %%% Global Skips [measure 3] %%%
                         \time 4/8
                         s1 * 1/2
-                        % measure 4
+            <BLANKLINE>
+                        %%% Global Skips [measure 4] %%%
                         \time 3/8
                         s1 * 3/8
+            <BLANKLINE>
                     }
                 >>
                 \context MusicContext = "Music Context" <<
                     \context Staff = "Music Staff" {
                         \context Voice = "Music Voice" {
                             {
-                                % measure 1
+            <BLANKLINE>
+                                %%% Music Voice [measure 1] %%%
                                 \clef "treble"
                                 c'8 [
+            <BLANKLINE>
                                 d'8
+            <BLANKLINE>
                                 e'8
+            <BLANKLINE>
                                 f'8 ]
                             }
                             {
-                                % measure 2
+            <BLANKLINE>
+                                %%% Music Voice [measure 2] %%%
                                 c''8 [
+            <BLANKLINE>
                                 d''8
+            <BLANKLINE>
                                 e''8 ]
                             }
                             {
-                                % measure 3
+            <BLANKLINE>
+                                %%% Music Voice [measure 3] %%%
                                 f''8 [
+            <BLANKLINE>
                                 c''8
+            <BLANKLINE>
                                 d''8
+            <BLANKLINE>
                                 e''8 ]
                             }
                             {
-                                % measure 4
+            <BLANKLINE>
+                                %%% Music Voice [measure 4] %%%
                                 f''8 [
+            <BLANKLINE>
                                 c''8
+            <BLANKLINE>
                                 d''8 ]
                                 \bar "|"
+            <BLANKLINE>
                             }
                         }
                     }

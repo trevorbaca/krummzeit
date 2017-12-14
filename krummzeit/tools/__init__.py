1c2273992c24ea7dd5e70546e0418f5dd710acb9
Trevor Bača <trevor.baca@gmail.com>
2017-12-14 12:12:15 -0600

    Cleaned up initializers.

--- krummzeit/tools/__init__.py
+++ krummzeit/tools/__init__.py
@@ -1,7 +1,8 @@
-import abjad
-
-
-abjad.ImportManager.import_structured_package(
-    __path__[0],
-    globals(),
-    )
+from .MarkupLibrary import MarkupLibrary
+from .RegisterTransitionCommand import RegisterTransitionCommand
+from .ScoreTemplate import ScoreTemplate
+from .clusters import clusters
+from .color_fingerings import color_fingerings
+from .displacement import displacement
+from .register_narrow import register_narrow
+from .register_wide import register_wide

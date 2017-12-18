343285f64e84c0f2bf2e7d10a47901f61bcff6e1
Trevor Bača <trevor.baca@gmail.com>
2017-12-18 15:38:46 -0600

    Cleaning up persistent indicator handling.

--- krummzeit/segments/A/__metadata__.py
+++ krummzeit/segments/A/__metadata__.py
@@ -3,9 +3,8 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
-        ('duration', "2'57''"),
         (
-            'end_clefs',
+            'abjad.Clef',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -40,7 +39,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_dynamics',
+            'abjad.Dynamic',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -67,7 +66,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_instruments',
+            'abjad.Instrument',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -101,9 +100,8 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_margin_markup', None),
         (
-            'end_metronome_marks',
+            'abjad.MetronomeMark',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -113,6 +111,19 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
+        (
+            'abjad.TimeSignature',
+            abjad.TypedOrderedDict(
+                [
+                    (
+                        'Score',
+                        ('5/4', 'GlobalSkips'),
+                        ),
+                    ]
+                ),
+            ),
+        ('baca.MarginMarkup', None),
+        ('duration', "2'57''"),
         (
             'end_staff_lines',
             abjad.TypedOrderedDict(
@@ -124,17 +135,6 @@ metadata = abjad.TypedOrderedDict(
                     (
                         'ViolinMusicStaff',
                         (1, 'ViolinMusicVoice'),
-                        ),
-                    ]
-                ),
-            ),
-        (
-            'end_time_signatures',
-            abjad.TypedOrderedDict(
-                [
-                    (
-                        'Score',
-                        ('5/4', 'GlobalSkips'),
                         ),
                     ]
                 ),
--- krummzeit/segments/D/__metadata__.py
+++ krummzeit/segments/D/__metadata__.py
@@ -3,9 +3,8 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
-        ('duration', "3'25''"),
         (
-            'end_clefs',
+            'abjad.Clef',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -40,7 +39,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_dynamics',
+            'abjad.Dynamic',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -75,7 +74,7 @@ metadata = abjad.TypedOrderedDict(
                 ),
             ),
         (
-            'end_instruments',
+            'abjad.Instrument',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -109,9 +108,8 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_margin_markup', None),
         (
-            'end_metronome_marks',
+            'abjad.MetronomeMark',
             abjad.TypedOrderedDict(
                 [
                     (
@@ -121,6 +119,19 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
+        (
+            'abjad.TimeSignature',
+            abjad.TypedOrderedDict(
+                [
+                    (
+                        'Score',
+                        ('9/8', 'GlobalSkips'),
+                        ),
+                    ]
+                ),
+            ),
+        ('baca.MarginMarkup', None),
+        ('duration', "3'25''"),
         (
             'end_staff_lines',
             abjad.TypedOrderedDict(
@@ -132,17 +143,6 @@ metadata = abjad.TypedOrderedDict(
                     (
                         'ViolinMusicStaff',
                         (5, 'ViolinMusicVoice'),
-                        ),
-                    ]
-                ),
-            ),
-        (
-            'end_time_signatures',
-            abjad.TypedOrderedDict(
-                [
-                    (
-                        'Score',
-                        ('9/8', 'GlobalSkips'),
                         ),
                     ]
                 ),

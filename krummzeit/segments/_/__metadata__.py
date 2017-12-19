d167b0b6440bcac5c228feeec38e4deea58e5e94
Trevor Bača <trevor.baca@gmail.com>
2017-12-19 12:41:56 -0600

    Cleaning up staff lines reapplication.

--- krummzeit/segments/_/__metadata__.py
+++ krummzeit/segments/_/__metadata__.py
@@ -98,6 +98,11 @@ metadata = abjad.TypedOrderedDict(
                                 prototype='abjad.Instrument',
                                 value='Percussion',
                                 ),
+                            abjad.Momento(
+                                context='PercussionMusicVoice',
+                                prototype='baca.StaffLines',
+                                value=1,
+                                ),
                             ],
                         ),
                     (

91a6ab2f403d6e661c3f6f3e766623e7e8559c07
Trevor Bača <trevor.baca@gmail.com>
2017-12-21 10:21:49 -0600

    Cleaning up instrument tags.

--- krummzeit/materials/instruments/definition.py
+++ krummzeit/materials/instruments/definition.py
@@ -6,73 +6,73 @@ instruments = abjad.InstrumentDictionary([
     (
         'BassClarinet',
         abjad.BassClarinet(
-            name_markup=baca.markup.instrument('Bass clarinet'),
-            short_name_markup=baca.markup.short_instrument('Bass cl.'),
+            markup=baca.markup.instrument('Bass clarinet'),
+            short_markup=baca.markup.short_instrument('Bass cl.'),
             )
         ),
     (
         'Cello',
         abjad.Cello(
-            name_markup=baca.markup.instrument('Cello'),
-            short_name_markup=baca.markup.short_instrument('Vc.'),
+            markup=baca.markup.instrument('Cello'),
+            short_markup=baca.markup.short_instrument('Vc.'),
             )
         ),
     (
         'ClarinetInEFlat',
         abjad.ClarinetInEFlat(
-            name_markup=baca.markup.instrument('Clarinet (Eb)'),
-            short_name_markup=baca.markup.short_instrument('Cl. (Eb)'),
+            markup=baca.markup.instrument('Clarinet (Eb)'),
+            short_markup=baca.markup.short_instrument('Cl. (Eb)'),
             )
         ),
     (
         'Harpsichord',
         abjad.Harpsichord(
             context='Staff',
-            name_markup=baca.markup.instrument('Harpsichord'),
-            short_name_markup=baca.markup.short_instrument('Hpschd.'),
+            markup=baca.markup.instrument('Harpsichord'),
+            short_markup=baca.markup.short_instrument('Hpschd.'),
             )
         ),
     (
         'Oboe',
         abjad.Oboe(
-            name_markup=baca.markup.instrument('Oboe'),
-            short_name_markup=baca.markup.short_instrument('Ob.'),
+            markup=baca.markup.instrument('Oboe'),
+            short_markup=baca.markup.short_instrument('Ob.'),
             )
         ),
     (
         'Percussion',
         abjad.Percussion(
-            name_markup=baca.markup.instrument('Percussion'),
-            short_name_markup=baca.markup.short_instrument('Perc.'),
+            markup=baca.markup.instrument('Percussion'),
+            short_markup=baca.markup.short_instrument('Perc.'),
             )
         ),
     (
         'Piano',
         abjad.Piano(
             context='Staff',
-            name_markup=baca.markup.instrument('Piano'),
-            short_name_markup=baca.markup.short_instrument('Pf.'),
+            markup=baca.markup.instrument('Piano'),
+            short_markup=baca.markup.short_instrument('Pf.'),
             )
         ),
     (
         'Viola',
         abjad.Viola(
-            name_markup=baca.markup.instrument('Viola'),
-            short_name_markup=baca.markup.short_instrument('Va.'),
+            markup=baca.markup.instrument('Viola'),
+            short_markup=baca.markup.short_instrument('Va.'),
             )
         ),
     (
         'Violin',
         abjad.Violin(
-            name_markup=baca.markup.instrument('Violin'),
-            short_name_markup=baca.markup.short_instrument('Vn.'),
+            markup=baca.markup.instrument('Violin'),
+            short_markup=baca.markup.short_instrument('Vn.'),
             )
         ),
     (
         'Xylophone',
         abjad.Xylophone(
-            name_markup=baca.markup.instrument('Xylophone'),
-            short_name_markup=baca.markup.short_instrument('Xyl.'),
+            markup=baca.markup.instrument('Xylophone'),
+            short_markup=baca.markup.short_instrument('Xyl.'),
             )
         ),
     ])

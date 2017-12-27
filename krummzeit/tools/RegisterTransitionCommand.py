80b3f21998906ba69816376a2c374c7de790a84a
Trevor Bača <trevor.baca@gmail.com>
2017-12-27 16:39:33 -0600

    Updated doctests.

--- krummzeit/tools/RegisterTransitionCommand.py
+++ krummzeit/tools/RegisterTransitionCommand.py
@@ -31,11 +31,11 @@ class RegisterTransitionCommand(baca.Command):
         ...     )
 
         >>> lilypond_file = maker.run(environment='docs')
-        >>> abjad.show(lilypond_file) # doctest: +SKIP
+        >>> abjad.show(lilypond_file, strict=79) # doctest: +SKIP
 
         ..  docs::
 
-            >>> abjad.f(lilypond_file[abjad.Score])
+            >>> abjad.f(lilypond_file[abjad.Score], strict=79)
             \context Score = "Score" <<
                 \context GlobalContext = "GlobalContext" <<
                     \context GlobalSkips = "GlobalSkips" {

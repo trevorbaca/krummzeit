75d1b6c4ada9b104c7148ca8c4871cab19bc0f90
Trevor Bača <trevor.baca@gmail.com>
2018-03-22 12:12:32 -0500

    Cleaning up definition files.

---
+++ krummzeit/tools/detached_triplets.py
@@ -0,0 +1,23 @@
+import abjad
+import baca
+from abjad import rhythmmakertools as rhythmos
+
+
+def detached_triplets():
+    r'''Makes detached triplets.
+    '''
+    return baca.RhythmCommand(
+        division_maker=baca.FuseByCountsDivisionCallback(
+            counts=abjad.Infinity,
+            secondary_division_maker=baca.SplitByDurationsDivisionCallback(
+                durations=[(1, 4)],
+                remainder=abjad.Right,
+                ),
+            ),
+        rhythm_maker=rhythmos.TupletRhythmMaker(
+            tuplet_ratios=[(3, -1, 2), (1, -1, 3, -1)],
+            tie_specifier=rhythmos.TieSpecifier(
+                tie_across_divisions=[1, 0],
+                ),
+            ),
+        )

ad900b1b14c691e09b0c05607bc34a9eda0f38f6
Trevor Bača <trevor.baca@gmail.com>
2018-06-04 12:42:53 -0500

    Integrated library changes.

--- krummzeit/segments/F/definition.py
+++ krummzeit/segments/F/definition.py
@@ -176,8 +176,8 @@ maker(
 maker(
     ('vn', (8, 10)),
     baca.transition(
-        baca.markup('grid. possibile'),
-        baca.markup('flaut. possibile'),
+        baca.markup.markup('grid. possibile'),
+        baca.markup.markup('flaut. possibile'),
         tweaks=[('staff-padding', 5)],
         ),
     )
@@ -185,8 +185,8 @@ maker(
 maker(
     (['va', 'vc'], (9, 10)),
     baca.transition(
-        baca.markup('grid. possibile'),
-        baca.markup('flaut. possibile'),
+        baca.markup.markup('grid. possibile'),
+        baca.markup.markup('flaut. possibile'),
         tweaks=[('staff-padding', 5)],
         ),
     )

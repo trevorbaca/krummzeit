0a7c6940b24d92ab73da4ee57b1e2b19e5914fb5
Trevor Bača <trevor.baca@gmail.com>
2018-06-05 09:29:27 -0500

    Integrated markup changes.

--- krummzeit/segments/D/definition.py
+++ krummzeit/segments/D/definition.py
@@ -327,8 +327,8 @@ maker(
     ('va', (5, 6)),
     baca.hairpin('pp < f'),
     baca.transition(
-        baca.markups.markup('molto flautando'),
-        baca.markups.markup('molto gridato'),
+        baca.markup('molto flautando'),
+        baca.markup('molto gridato'),
         tweaks=[('staff-padding', 6)],
         ),
     )
@@ -337,8 +337,8 @@ maker(
     (['vn', 'vc'], 6),
     baca.hairpin('pp < f'),
     baca.transition(
-        baca.markups.markup('molto flautando'),
-        baca.markups.markup('molto gridato'),
+        baca.markup('molto flautando'),
+        baca.markup('molto gridato'),
         tweaks=[('staff-padding', 6)],
         ),
     )

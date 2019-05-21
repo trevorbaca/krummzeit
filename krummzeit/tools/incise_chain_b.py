50814bea3911f625727eea6dd5ccfb916e799fde
Trevor Bača <trevor.baca@gmail.com>
2019-05-21 11:18:08 -0500

    Updated incised rhythms.

--- krummzeit/tools/incise_chain_b.py
+++ krummzeit/tools/incise_chain_b.py
@@ -11,12 +11,12 @@ def incise_chain_b():
         rhythm_maker=rmakers.IncisedRhythmMaker(
             beam_specifier=rmakers.BeamSpecifier(beam_each_division=False),
             incise_specifier=rmakers.InciseSpecifier(
+                fill_with_rests=True,
                 prefix_talea=[-2, 2, 3, -2, 3, -2, 2],
                 prefix_counts=[3, 2, 2],
                 suffix_talea=[2, -2, 3, -1, 0, 2, -2],
                 suffix_counts=[2, 2, 1, 2],
                 talea_denominator=16,
-                fill_with_notes=False,
             ),
             tag="incise_chain_b",
         )

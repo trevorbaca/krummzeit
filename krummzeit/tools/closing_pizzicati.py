9b8c2dee0ad5bebf8aa612dbf1cdd26a2a440749
Trevor Bača <trevor.baca@gmail.com>
2019-06-12 16:25:58 -0400

    Cleaned up tuplet specifiers.

--- krummzeit/tools/closing_pizzicati.py
+++ krummzeit/tools/closing_pizzicati.py
@@ -16,11 +16,11 @@ def closing_pizzicati(
     return baca.rhythm(
         divisions=divisions,
         rhythm_maker=rmakers.TaleaRhythmMaker(
+            rmakers.TupletSpecifier(extract_trivial=True),
             beam_specifier=rmakers.BeamSpecifier(beam_each_division=True),
             extra_counts_per_division=extra_counts,
             rest_tied_notes=True,
             talea=rmakers.Talea(counts=counts, denominator=4),
-            tuplet_specifier=rmakers.TupletSpecifier(extract_trivial=True),
         ),
         tag="krummzeit.closing_pizzicati",
     )

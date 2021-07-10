c52d78d921e835528151002e85111d23a99c6c69
Trevor Bača <trevor.baca@gmail.com>
2021-07-10 15:26:47 -0400

    Cleaned up fermata measure number metadata.

--- krummzeit/__metadata__.py
+++ krummzeit/__metadata__.py
@@ -4,45 +4,6 @@ metadata = abjad.OrderedDict(
     [
         ("catalog_number", "AWN-008"),
         ("composer", "Trevor Bača"),
-        (
-            "fermata_measure_numbers",
-            abjad.OrderedDict(
-                [
-                    (
-                        "_",
-                        [3, 9],
-                    ),
-                    (
-                        "B",
-                        [
-                            93,
-                            95,
-                            97,
-                            99,
-                            101,
-                            103,
-                            105,
-                            107,
-                            109,
-                            117,
-                            119,
-                        ],
-                    ),
-                    (
-                        "C",
-                        [134, 136, 138, 140, 145],
-                    ),
-                    (
-                        "G",
-                        [269, 280],
-                    ),
-                    (
-                        "I",
-                        [313],
-                    ),
-                ]
-            ),
-        ),
         ("forces_tagline", "for seven players"),
         ("title", "Krummzeit"),
         ("year", 2014),

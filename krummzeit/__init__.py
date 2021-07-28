d99b2f3f279482ab5727d708ce92c1d143e5d995
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:50:00 -0400

    Taught package initializer to check for Python 3.9 or later.

--- krummzeit/__init__.py
+++ krummzeit/__init__.py
@@ -1,5 +1,4 @@
-import distutils.version
-import platform
+import sys
 
 from .library import (
     RegisterTransitionCommand,
@@ -87,10 +86,6 @@ __all__ = [
     "white_rhythm",
 ]
 
-if not (
-    distutils.version.LooseVersion("3.7")
-    < distutils.version.LooseVersion(platform.python_version())
-):
-    raise ImportError("Requires Python 3.7.")
-del distutils
-del platform
+if sys.version_info[:2] < (3, 9):
+    raise ImportError("Requires Python 3.9 or later")
+del sys

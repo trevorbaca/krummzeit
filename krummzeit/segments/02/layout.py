6796d2972e1fb146a10a413e8258149130cbe7f9
Trevor Bača <trevor.baca@gmail.com>
2021-08-08 11:03:05 -0400

    Cleaned up layout files.

--- krummzeit/segments/02/layout.py
+++ krummzeit/segments/02/layout.py
@@ -10,67 +10,67 @@ distances = (
 
 breaks = baca.breaks(
     baca.page(
-        baca.system(measure=14, y_offset=y_offset, distances=distances),
+        baca.system(measure=1, y_offset=y_offset, distances=distances),
         number=1,
     ),
     baca.page(
-        baca.system(measure=19, y_offset=y_offset, distances=distances),
+        baca.system(measure=6, y_offset=y_offset, distances=distances),
         number=2,
     ),
     baca.page(
-        baca.system(measure=23, y_offset=y_offset, distances=distances),
+        baca.system(measure=10, y_offset=y_offset, distances=distances),
         number=3,
     ),
     baca.page(
-        baca.system(measure=29, y_offset=y_offset, distances=distances),
+        baca.system(measure=16, y_offset=y_offset, distances=distances),
         number=4,
     ),
     baca.page(
-        baca.system(measure=33, y_offset=y_offset, distances=distances),
+        baca.system(measure=20, y_offset=y_offset, distances=distances),
         number=5,
     ),
     baca.page(
-        baca.system(measure=38, y_offset=y_offset, distances=distances),
+        baca.system(measure=25, y_offset=y_offset, distances=distances),
         number=6,
     ),
     baca.page(
-        baca.system(measure=42, y_offset=y_offset, distances=distances),
+        baca.system(measure=29, y_offset=y_offset, distances=distances),
         number=7,
     ),
     baca.page(
-        baca.system(measure=46, y_offset=y_offset, distances=distances),
+        baca.system(measure=33, y_offset=y_offset, distances=distances),
         number=8,
     ),
     baca.page(
-        baca.system(measure=51, y_offset=y_offset, distances=distances),
+        baca.system(measure=38, y_offset=y_offset, distances=distances),
         number=9,
     ),
     baca.page(
-        baca.system(measure=55, y_offset=y_offset, distances=distances),
+        baca.system(measure=42, y_offset=y_offset, distances=distances),
         number=10,
     ),
     baca.page(
-        baca.system(measure=60, y_offset=y_offset, distances=distances),
+        baca.system(measure=47, y_offset=y_offset, distances=distances),
         number=11,
     ),
     baca.page(
-        baca.system(measure=65, y_offset=y_offset, distances=distances),
+        baca.system(measure=52, y_offset=y_offset, distances=distances),
         number=12,
     ),
     baca.page(
-        baca.system(measure=70, y_offset=y_offset, distances=distances),
+        baca.system(measure=57, y_offset=y_offset, distances=distances),
         number=13,
     ),
     baca.page(
-        baca.system(measure=76, y_offset=y_offset, distances=distances),
+        baca.system(measure=63, y_offset=y_offset, distances=distances),
         number=14,
     ),
     baca.page(
-        baca.system(measure=81, y_offset=y_offset, distances=distances),
+        baca.system(measure=68, y_offset=y_offset, distances=distances),
         number=15,
     ),
     baca.page(
-        baca.system(measure=86, y_offset=y_offset, distances=distances),
+        baca.system(measure=73, y_offset=y_offset, distances=distances),
         number=16,
     ),
 )

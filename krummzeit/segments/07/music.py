8dcab3b9f200765b920a7b1f38d25a2761d83b44
Trevor Bača <trevor.baca@gmail.com>
2021-10-05 12:18:29 +0200

    Bundled LilyPond file creation keywords.

--- krummzeit/segments/07/music.py
+++ krummzeit/segments/07/music.py
@@ -353,6 +353,10 @@ if __name__ == "__main__":
         activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
         always_make_global_rests=True,
         error_on_not_yet_pitched=True,
+        lilypond_file_keywords=baca.make_lilypond_file_dictionary(
+            include_layout_ly=True,
+            includes=["../../stylesheet.ily"],
+        ),
         score=score,
         stage_markup=stage_markup,
         transpose_score=True,

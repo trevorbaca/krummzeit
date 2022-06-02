1bd6a0d5e182d2fdef4ac0b347f4f89da1e4a0d5
Trevor Bača <trevor.baca@gmail.com>
2022-06-02 12:42:33 +0200

    Cleaned up instrument dictionaries.

--- krummzeit/library.py
+++ krummzeit/library.py
@@ -339,17 +339,17 @@ def instruments():
     return dict(
         [
             ("BassClarinet", abjad.BassClarinet()),
-            ("Cello", abjad.Cello(pitch_range="[A1, +inf]")),
+            ("Cello", abjad.Cello(pitch_range=abjad.PitchRange("[A1, +inf]"))),
             ("ClarinetInEFlat", abjad.ClarinetInEFlat()),
             ("Harpsichord", abjad.Harpsichord(context="Staff")),
-            ("Oboe", abjad.Oboe(pitch_range="[Bb3, Bb6]")),
+            ("Oboe", abjad.Oboe(pitch_range=abjad.PitchRange("[Bb3, Bb6]"))),
             ("Percussion", abjad.Percussion()),
             ("Piano", abjad.Piano(context="Staff")),
-            ("Viola", abjad.Viola(pitch_range="[Bb2, +inf]")),
+            ("Viola", abjad.Viola(pitch_range=abjad.PitchRange("[Bb2, +inf]"))),
             (
                 "Violin",
                 # TODO: F#3 instead of F3
-                abjad.Violin(pitch_range="[F3, +inf]"),
+                abjad.Violin(pitch_range=abjad.PitchRange("[F3, +inf]")),
             ),
             ("Xylophone", abjad.Xylophone()),
         ]

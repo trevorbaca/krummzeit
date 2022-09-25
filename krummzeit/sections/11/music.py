23852753c48b80ef9236cb0775bf461839ac3900
Trevor Bača <trevor.baca@gmail.com>
2022-09-25 18:18:56 -0400

    Cleaned up music.py files.

--- krummzeit/sections/11/music.py
+++ krummzeit/sections/11/music.py
@@ -221,7 +221,7 @@ def main():
     )
     defaults = baca.section.section_defaults()
     del defaults["append_anchor_skip"]
-    metadata, persist, timing = baca.build.section(
+    metadata, persist, timing = baca.build.postprocess_score(
         score,
         library.manifests,
         accumulator.time_signatures,
@@ -238,7 +238,7 @@ def main():
         include_layout_ly=True,
         includes=["../stylesheet.ily"],
     )
-    baca.build.persist(
+    baca.build.persist_lilypond_file(
         lilypond_file,
         environment.metadata,
         environment.persist,

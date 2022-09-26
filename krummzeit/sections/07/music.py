64c06195e95da37d89349429477c82e2d1d456d8
Trevor Bača <trevor.baca@gmail.com>
2022-09-26 08:28:00 -0400

    Cleaned up timing.

--- krummzeit/sections/07/music.py
+++ krummzeit/sections/07/music.py
@@ -304,6 +304,7 @@ def ob_cl_4_10(cache):
             baca.hairpin(o, "p < ff")
 
 
+@baca.build.timed
 def make_score(first_measure_number, previous_persistent_indicators):
     score, accumulator = make_empty_score()
     baca.section.set_up_score(
@@ -347,9 +348,11 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
+    timing = baca.build.Timing()
     score, accumulator = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
+        timing,
     )
     metadata, persist, timing = baca.build.postprocess_score(
         score,

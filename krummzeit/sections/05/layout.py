81308c5b8deaac8f1e1c5f1408e2c619c0317bae
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:15:55 -0400

    Cleaned up layout.py files.

--- krummzeit/sections/05/layout.py
+++ krummzeit/sections/05/layout.py
@@ -3,53 +3,57 @@ import baca
 y_offset = 20
 distances = (20, (20, 30), (20, 30), (20, 20, 30))
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        2,
-        baca.system(measure=6, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        3,
-        baca.system(measure=10, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        4,
-        baca.system(measure=15, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        5,
-        baca.system(measure=19, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        6,
-        baca.system(measure=24, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        7,
-        baca.system(measure=28, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        8,
-        baca.system(measure=32, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        9,
-        baca.system(measure=36, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        10,
-        baca.system(measure=41, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        11,
-        baca.system(measure=45, y_offset=y_offset, distances=distances),
-    ),
-    spacing=(1, 40),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            2,
+            baca.system(measure=6, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            3,
+            baca.system(measure=10, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            4,
+            baca.system(measure=15, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            5,
+            baca.system(measure=19, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            6,
+            baca.system(measure=24, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            7,
+            baca.system(measure=28, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            8,
+            baca.system(measure=32, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            9,
+            baca.system(measure=36, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            10,
+            baca.system(measure=41, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            11,
+            baca.system(measure=45, y_offset=y_offset, distances=distances),
+        ),
+        spacing=(1, 40),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
--- krummzeit/sections/10/layout.py
+++ krummzeit/sections/10/layout.py
@@ -3,29 +3,33 @@ import baca
 y_offset = 20
 distances = (20, (20, 30), (20, 30), (20, 20, 30))
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        2,
-        baca.system(measure=5, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        3,
-        baca.system(measure=9, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        4,
-        baca.system(measure=13, y_offset=y_offset, distances=distances),
-    ),
-    baca.page(
-        5,
-        baca.system(measure=18, y_offset=y_offset, distances=distances),
-    ),
-    spacing=(1, 40),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            2,
+            baca.system(measure=5, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            3,
+            baca.system(measure=9, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            4,
+            baca.system(measure=13, y_offset=y_offset, distances=distances),
+        ),
+        baca.page(
+            5,
+            baca.system(measure=18, y_offset=y_offset, distances=distances),
+        ),
+        spacing=(1, 40),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()

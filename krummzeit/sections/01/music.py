e1c597c3cdd14d2d31db8b6ec40f5723b0be0fe1
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 14:04:17 -0400

    Cleaned up music.py files.

--- krummzeit/sections/01/music.py
+++ krummzeit/sections/01/music.py
@@ -296,15 +296,6 @@ def make_score():
     return score
 
 
-def main():
-    environment = baca.build.read_environment(__file__, baca.build.argv())
-    if environment.score():
-        score = make_score(environment.timing)
-        persist_score(score, environment)
-    if environment.arguments.layout:
-        make_layout()
-
-
 def persist_score(score, environment):
     metadata = baca.section.postprocess_score(
         score,
@@ -352,6 +343,15 @@ def make_layout():
         overrides=(baca.space((10, 13), (1, 24)),),
     )
     baca.section.make_layout_ly(spacing)
+
+
+def main():
+    environment = baca.build.read_environment(__file__, baca.build.argv())
+    if environment.score():
+        score = make_score(environment.timing)
+        persist_score(score, environment)
+    if environment.arguments.layout:
+        make_layout()
 
 
 if __name__ == "__main__":

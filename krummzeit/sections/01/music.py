d58a894ccf5d03e95609d771be595d18a0244582
Trevor Bača <trevor.baca@gmail.com>
2023-09-22 14:32:35 -0400

    Cleaned up music.py files.

--- krummzeit/sections/01/music.py
+++ krummzeit/sections/01/music.py
@@ -269,7 +269,6 @@ def make_score():
         score,
         time_signatures(),
         append_anchor_skip=True,
-        always_make_global_rests=True,
         manifests=library.manifests,
     )
     GLOBALS(score["Skips"], score["Rests"])
@@ -301,7 +300,6 @@ def persist_score(score, environment):
         score,
         environment,
         library.manifests,
-        always_make_global_rests=True,
         error_on_not_yet_pitched=True,
         first_section=True,
         transpose_score=True,

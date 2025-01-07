8c962da6f45c619020b68098f059ecc038060231
Trevor Bača <trevor.baca@gmail.com>
2025-01-07 15:43:58 -0500

    Clean up __init__.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         name="krummzeit",
         packages=["krummzeit"],
         platforms="Any",
-        python_requires=">=3.10",
+        python_requires=">=3.12",
         url="https://github.com/trevorbaca/krummzeit",
         version="3.19",
     )
